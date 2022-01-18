af9ec947e9a4f90d8572e81920ff718c0f56a4d7
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-18 13:12:08 +0800

    Added addWalletListener function

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -77,4 +77,28 @@ const Minter = (props) => {
   );
 };
 
+function addWalletListener() {
+  if (window.ethereum) {
+    window.ethereum.on("accountsChanged", (accounts) => {
+      if (accounts.length > 0) {
+        setWallet(accounts[0]);
+        setStatus("👆🏽 Write a message in the text field above.");
+      } else {
+        setWallet("");
+        setStatus("🦊 Connect to MetaMask using the top right button.");
+      }
+    });
+  } else {
+    setStatus(
+      <p>
+        {" "}
+        🦊{" "}
+        <a target="_blank" href={`https://metamask.io/download.html`}>
+          You must install MetaMask, a virtual Ethereum wallet, in your browser.
+        </a>
+      </p>
+    );
+  }
+}
+
 export default Minter;
