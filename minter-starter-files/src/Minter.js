6c2aca97e437c9ac978ee78cb4bddfdd2b504dd8
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-18 14:45:11 +0800

    Refactored Minter.js

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -21,6 +21,30 @@ const Minter = (props) => {
     addWalletListener();
   }, []);
 
+  function addWalletListener() {
+    if (window.ethereum) {
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("👆🏽 Write a message in the text field above.");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to MetaMask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install MetaMask, a virtual Ethereum wallet, in your browser.
+          </a>
+        </p>
+      );
+    }
+  }
+
   const connectWalletPressed = async () => { //TODO: implement
     const walletResponse = await connectWallet();
     setStatus(walletResponse.status);
@@ -78,29 +102,5 @@ const Minter = (props) => {
     </div>
   );
 };
-
-function addWalletListener() {
-  if (window.ethereum) {
-    window.ethereum.on("accountsChanged", (accounts) => {
-      if (accounts.length > 0) {
-        setWallet(accounts[0]);
-        setStatus("👆🏽 Write a message in the text field above.");
-      } else {
-        setWallet("");
-        setStatus("🦊 Connect to MetaMask using the top right button.");
-      }
-    });
-  } else {
-    setStatus(
-      <p>
-        {" "}
-        🦊{" "}
-        <a target="_blank" href={`https://metamask.io/download.html`}>
-          You must install MetaMask, a virtual Ethereum wallet, in your browser.
-        </a>
-      </p>
-    );
-  }
-}
 
 export default Minter;
