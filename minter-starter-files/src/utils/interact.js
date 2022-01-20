c09b48578334fd47650f214a9ddbc435e83a29c0
Dante O. Cuales <danteocualesjr@gmail.com>
2022-01-20 17:59:56 +0800

    Updated mintNFT function

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -41,9 +41,10 @@ export const connectWallet = async () => {
             ),
         };
     }
-};
+}
 
 export const mintNFT = async (url, name, description) => {
+    
     // Error handling
     if (url.trim() == "" || (name.trim() == "" || description.trim() == "")) {
         return {
