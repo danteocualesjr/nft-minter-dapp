a41e22c36e2e3b804ea3392af2a61ca50420ef10
Dante O. Cuales <danteocualesjr@gmail.com>
2022-06-07 08:10:56 +0800

    Refactoring

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import {   
+  
   connectWallet,
   getCurrentWalletConnected,
   mintNFT    
@@ -19,7 +20,7 @@ const Minter = (props) => {
     setWallet(address);
     setStatus(status);    
     addWalletListener(); 
-    
+
   }, []);
 
   function addWalletListener() {
