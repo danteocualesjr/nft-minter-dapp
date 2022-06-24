35f09913d0843f75e3ac6d263513ca61af1c8892
Dante O. Cuales <danteocualesjr@gmail.com>
2022-06-24 08:34:14 +0800

    Refactored code

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -10,7 +10,6 @@ import {
 const Minter = (props) => {  
 
   //State variables  
-
   const [walletAddress, setWallet] = useState("");
   const [status, setStatus] = useState("");
   const [name, setName] = useState("");
