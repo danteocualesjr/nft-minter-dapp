1c7ca0452df13536a06c2542318e3a936c3171b9
Dante O. Cuales <danteocualesjr@gmail.com>
2022-07-18 17:33:44 +0800

    Refactored code

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -27,6 +27,7 @@ const Minter = (props) => {
   }, []);
 
   function addWalletListener() {
+    
     if (window.ethereum) {
       window.ethereum.on("accountsChanged", (accounts) => {
         if (accounts.length > 0) {
