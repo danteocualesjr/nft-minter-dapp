c504574603c848935e9ed2a79703293394958be1
Dante O. Cuales <danteocualesjr@gmail.com>
2022-07-22 14:47:31 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -5,6 +5,7 @@ require('dotenv').config();
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+
 const web3 = createAlchemyWeb3(alchemyKey);
 
 const contractABI = require('../contract-abi.json');
