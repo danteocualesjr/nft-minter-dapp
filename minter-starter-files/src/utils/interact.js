1d0f5d2c41786d3d0587ae66a17eb050e851574b
Dante O. Cuales <danteocualesjr@gmail.com>
2022-07-31 10:07:58 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -3,6 +3,7 @@ import { pinJSONToIPFS } from './pinata.js';
 require('dotenv').config();
 
 const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 const contractABI = require('../contract-abi.json');
