00ca7e2840d1061a51e961b198d2299fb8f0ac32
Dante O. Cuales <danteocualesjr@gmail.com>
2022-08-21 07:53:01 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -8,7 +8,6 @@ const contractABI = require('../contract-abi.json');
 const contractAddress = "0xe8b00e6d6de63a9c4d3d3b881d2199505eb03c5c";
 
 export const connectWallet = async () => {
-
     if (window.ethereum) {
         try { 
             const addressArray = await window.ethereum.request({
