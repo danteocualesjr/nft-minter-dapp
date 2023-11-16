dadf6e370846d9f694a6194090c6a3160f59ead3
Dante O. Cuales <danteocualesjr@gmail.com>
2023-11-16 17:59:50 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -224,6 +224,7 @@ export const getCurrentWalletConnected = async () => {
                 <span>
                     
                     <p>
+                        
                         {" "}
                         🦊{" "}
                         <a target ="_blank" href={`https://metamask.io/download.html`}>
