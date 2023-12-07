1db3e21eba79166008f05d9df41fb5562ffe92d8
Dante O. Cuales <danteocualesjr@gmail.com>
2023-12-07 08:14:13 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -17,9 +17,7 @@ export const connectWallet = async () => {
                 status : "👆🏽 Write a message in the text field above.",
                 address: addressArray[0],
             };
-            
             return obj;
-
          } catch (err) {
              
             return {
