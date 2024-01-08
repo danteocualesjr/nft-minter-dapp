d761bcbf8e38ee218b07bfc5433b993545e0539d
Dante O. Cuales <danteocualesjr@gmail.com>
2024-01-08 08:02:23 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -107,7 +107,6 @@ export const mintNFT = async (url, name, description) => {
 export const getCurrentWalletConnected = async () => {
     if (window.ethereum) {
         try {
-
             const addressArray = await window.ethereum.request({
                 
                 method: "eth.accounts",
