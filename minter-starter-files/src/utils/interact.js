959ffb954477fd65548f785d0e299d5139ebaa42
Dante O. Cuales <danteocualesjr@gmail.com>
2024-01-14 22:11:28 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -124,7 +124,6 @@ export const getCurrentWalletConnected = async () => {
         } catch (err) {
             return {
                 address: "",
-
                 status: "😥 " + err.message,
                 
             };
