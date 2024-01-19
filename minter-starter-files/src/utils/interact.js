fab56d95d7ad9a00753c85ea1025cc6d7ea92118
Dante O. Cuales <danteocualesjr@gmail.com>
2024-01-19 17:11:46 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -138,7 +138,6 @@ export const getCurrentWalletConnected = async () => {
                         🦊{" "}
                         <a target ="_blank" href={`https://metamask.io/download.html`}>
                             You must install MetaMask, a virtual Ethereum wallet, in your browser.
-
                         </a>
 
                     </p>
