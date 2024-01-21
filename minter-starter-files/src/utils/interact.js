3416a9fa1b29b4fd58f12888dd07dc25eabeaef9
Dante O. Cuales <danteocualesjr@gmail.com>
2024-01-21 17:01:37 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -9,6 +9,7 @@ const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
 const contractABI = require('../contract-abi.json');
+
 const contractAddress = "0xe8b00e6d6de63a9c4d3d3b881d2199505eb03c5c";
 
 export const connectWallet = async () => {
