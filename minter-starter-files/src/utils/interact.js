761c3494a9c0e3a9bd53e87336cb2cd366eb5783
Dante O. Cuales <danteocualesjr@gmail.com>
2024-02-26 16:42:37 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -181,8 +181,9 @@ export const getCurrentWalletConnected = async () => {
         try {
             
             const addressArray = await window.ethereum.request({ 
-                
+
                 method: "eth.accounts",
+                
             });
             if (addressArray.length > 0) {
                 return {
