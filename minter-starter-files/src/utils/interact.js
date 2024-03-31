e1e48462868a5021eceaf812c374f00d95eb4571
Dante O. Cuales <danteocualesjr@gmail.com>
2024-04-01 06:40:31 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -46,7 +46,6 @@ export const mintNFT = async (url, name, description) => {
 
     // Error handling
     if (url.trim() == "" || (name.trim() == "" || description.trim() == "")) {
-        
         return {
 
             success: false,
