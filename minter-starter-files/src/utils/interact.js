b43adc226297e16874216284e1e677af8c49b1e8
Dante O. Cuales <danteocualesjr@gmail.com>
2024-04-05 18:15:30 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -62,7 +62,6 @@ export const mintNFT = async (url, name, description) => {
     const pinataResponse = await pinJSONToIPFS(metadata);
     if (!pinataResponse.success) {
         return {
-            
             success: false,
 
             status: "😢 Something went wrong while uploading your tokenURI.",
