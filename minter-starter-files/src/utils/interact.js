f81ad021ccfd5ea8e4c95f1e0bc8d442d6a73d39
Dante O. Cuales <danteocualesjr@gmail.com>
2024-04-07 22:55:04 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -74,7 +74,6 @@ export const mintNFT = async (url, name, description) => {
     // Setting up Ethereum transaction
     const transactionParameters = {
         to: contractAddress, // Required except during contract publications
-
         from: window.ethereum.selectedAddress, // Must match user's active address
 
         'data': window.contract.methods.mintNFT(window.ethereum.selectedAddress, tokenURI).encodeABI() // Make call to NFT smart contract
