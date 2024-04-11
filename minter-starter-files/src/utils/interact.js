94f47cca0b71a0e021117b08613c862fd984c960
Dante O. Cuales <danteocualesjr@gmail.com>
2024-04-12 07:11:49 +0800

    Refactored code

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -83,7 +83,6 @@ export const mintNFT = async (url, name, description) => {
         const txHash = await window.ethereum
             .request({
                 method: 'eth_sendTransaction',
-
                 params: [transactionParameters],
                 
             });
