77e67ccfcb6e2631cfbbe7a6da5813a0cea95756
Dante O. Cuales <danteocualesjr@gmail.com>
2024-05-10 08:30:53 +0800

    Refactored code

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -21,10 +21,11 @@ const Minter = (props) => {
   const [name, setName] = useState("");
 
   const [description, setDescription] = useState("");
-  
+
   const [url, setURL] = useState("");
  
   useEffect(async () => { //TODO: implement
+    
     const { address, status } = await getCurrentWalletConnected();
     setWallet(address);
     setStatus(status);    
