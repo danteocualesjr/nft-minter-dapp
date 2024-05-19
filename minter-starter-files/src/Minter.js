874e862846348a056d8ffcbb2cdc79c7a040b89c
Dante O. Cuales <danteocualesjr@gmail.com>
2024-05-19 10:14:39 +0800

    Refactored code

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -67,8 +67,9 @@ const Minter = (props) => {
           {" "}
 
           🦊{" "}
-          
+
           <a target="_blank" href={`https://metamask.io/download.html`}>
+          
             You must install MetaMask, a virtual Ethereum wallet, in your browser.
           </a>
         </p>
