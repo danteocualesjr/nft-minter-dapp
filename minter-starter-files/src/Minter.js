b59973afc5a6956217d7ca740a6b4242908b7e86
Dante O. Cuales <danteocualesjr@gmail.com>
2024-05-27 14:32:12 +0800

    Refactored code

--- minter-starter-files/src/Minter.js
+++ minter-starter-files/src/Minter.js
@@ -105,6 +105,7 @@ const Minter = (props) => {
     <div className="Minter">
       
       <button id="walletButton" onClick={connectWalletPressed}>
+      
         {walletAddress.length > 0 ? (
           "Connected: " +
           String(walletAddress).substring(0, 6) +
